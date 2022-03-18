46d36b4f2fea2336ed99280be86405240d5d55cb
DalkomCandy <gyumin0815@gmail.com>
2022-03-18 11:26:57 +0900

    2022-03-18

--- app/src/main/java/com/example/teardrop/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/teardrop/AddHappyPlaceActivity.kt
@@ -5,12 +5,16 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.teardrop.R.layout.activity_add_happy_place
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
@@ -27,11 +31,17 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 import com.karumi.dexter.listener.single.PermissionListener
-
-
+import java.io.IOException
 
 
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "TearDropImages"
+        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 3
+    }
 
     private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
@@ -52,8 +62,9 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
             updateDateInView()
         }
+
         et_date.setOnClickListener(this) // this를 쓰는 이유는 AddHappyPlaceActivity 자체가 OnClicklister
-        tv_add_image
+        tv_add_image.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -76,7 +87,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
                 pictureDialog.setItems(pictureDialogItems) {
                         _, which ->
-                    when(which){
+                    when (which){
                         0 -> choosePhotoFromGallery()
                         1 -> Toast.makeText(
                         this@AddHappyPlaceActivity,
@@ -84,22 +95,50 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                         Toast.LENGTH_SHORT).show()
                     }
                 }
+                pictureDialog.show()
             }
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentUri = data.data
+                    try {
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentUri)
+
+                        iv_place_image.setImageBitmap(selectedImageBitmap)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(
+                            this@AddHappyPlaceActivity,
+                            "Failed to Load the image from",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            } else if (requestCode == CAMERA) {
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                iv_place_image.setImageBitmap(thumbnail)
+            }
+        }
+    }
+
+
     private fun choosePhotoFromGallery() {
         Dexter.withContext(this).withPermissions(
             android.Manifest.permission.READ_EXTERNAL_STORAGE,
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE
         ).withListener(object : MultiplePermissionsListener {
-            override fun onPermissionsChecked(
-                report : MultiplePermissionsReport) {
-                if(report.areAllPermissionsGranted()){
-                    Toast.makeText(
-                        this@AddHappyPlaceActivity,
-                        "권한 허용됨."
-                        , Toast.LENGTH_SHORT).show()
+            override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                if (p0!!.areAllPermissionsGranted()) {
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(galleryIntent, GALLERY)
                 }
             }
 
@@ -136,3 +175,4 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         et_date.setText(sdf.format(cal.time).toString())
     }
 }
+
